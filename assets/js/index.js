75200c03e5cba11edc674c64eeaed3c6a35e517a
qianduan <mienflying@gmail.com>
2015-04-08 22:38:02 +0800

    modified search box

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,18 @@
     });
 
 })(jQuery);
+
+function search() {
+    var q = document.getElementById("search-field");
+    if (q.value != "") {
+        var url = 'https://www.google.com.hk/search?q=site:qianduan.net%20' + q.value;
+        if (navigator.userAgent.indexOf('iPad') > -1 || navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPhone') > -1) {
+            location.href = url;
+        } else {
+            window.open(url, "_blank");
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
