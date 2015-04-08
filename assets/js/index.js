e78ca8e860a3b160c372953c73d5fa0d9e95a6c9
qianduan <mienflying@gmail.com>
2015-04-08 21:44:35 +0800

    optimize details

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 /**
- * Main JS file for Casper behaviours
+ * Main JS file for Next theme behaviours
  */
 
 /* globals jQuery, document */
@@ -53,4 +53,5 @@
         });
 
     };
+    $('.related-posts').ghostRelated();
 })(jQuery);
